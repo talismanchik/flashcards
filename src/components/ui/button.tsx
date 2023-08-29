5dc2b0889523f1594aa7a6a2f9ff245b1d895d03
talismanchik <talismanchik.by@gmail.com>
2023-08-29 20:04:00 +0300

    feat: create button

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,14 @@
+import { ComponentPropsWithoutRef } from 'react'
+
+import s from './button.module.scss'
+
+export type ButtonProps = {
+  variant?: 'primary' | 'secondary' | 'tertiary' | 'link'
+  fullWidth?: boolean
+} & ComponentPropsWithoutRef<'button'>
+
+export const Button = ({ variant = 'primary', fullWidth, className, ...rest }: ButtonProps) => {
+  return (
+    <button className={`${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`} {...rest} />
+  )
+}
