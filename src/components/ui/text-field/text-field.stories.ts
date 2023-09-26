1757d35c54a6db315a324ae9a56a8d2f6c806487
GmyryaAnastasia <nastgm26@gmail.com>
2023-09-26 22:42:27 +0300

    add textfield

---
+++ src/components/ui/text-field/text-field.stories.ts
@@ -0,0 +1,59 @@
+import {Meta} from "@storybook/blocks";
+import {StoryObj} from "@storybook/react";
+import {TextField} from "@/components/ui/text-field/text-field";
+
+const meta = {
+    title: 'Components/TextField',
+    component: TextField,
+    tags: ['autodocs'],
+    argTypes: {
+        variant: {
+            options: ['text', 'password', 'search','disabled','error'],
+        },
+    },
+}satisfies Meta<typeof TextField>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+
+export const Text: Story = {
+    args: {
+        label: 'Label',
+        placeholder: 'Placeholder',
+    },
+}
+
+export const Password: Story = {
+    args: {
+        label: 'Label',
+        placeholder: 'Placeholder',
+        type:'password'
+    },
+}
+
+export const Search: Story = {
+    args: {
+        label: 'Label',
+        placeholder: 'Placeholder',
+        type:'search'
+    },
+}
+
+export const Disabled: Story = {
+    args: {
+        label: 'Label',
+        placeholder: 'Placeholder',
+        type:'text',
+        disabled:true
+    },
+}
+
+export const Error: Story = {
+    args: {
+        label: 'Label',
+        placeholder: 'Placeholder',
+        type:'text',
+        errorMessage:'ErrorMessage'
+    },
+}
